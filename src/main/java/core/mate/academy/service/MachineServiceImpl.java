258e6052343aa4cb02cc8c34b54156aaaf96b5a8
Karol Gajda <karolgaj97@interia.pl>
2024-09-10 15:09:36 +0200

    adding parametrization, simpify getall method

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,9 +1,6 @@
 package core.mate.academy.service;
 
-import core.mate.academy.model.Bulldozer;
-import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
-import core.mate.academy.model.Truck;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,19 +10,16 @@ import java.util.List;
 public class MachineServiceImpl implements MachineService<Machine> {
 
     @Override
-    public List<Machine> getAll(Class type) {
-        List<Machine> machineList = new ArrayList<>();
-        if (type.equals(Bulldozer.class)) {
-            BulldozerProducer bulldozerProducer = new BulldozerProducer();
-            machineList = new ArrayList<>(bulldozerProducer.get());
-        } else if (type.equals(Excavator.class)) {
-            ExcavatorProducer excavatorProducer = new ExcavatorProducer();
-            machineList = new ArrayList<>(excavatorProducer.get());
-        } else if (type.equals(Truck.class)) {
-            TruckProducer truckproducer = new TruckProducer();
-            machineList = new ArrayList<>(truckproducer.get());
-        }
-        return machineList;
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        List<? extends Machine> machineList;
+        switch (type.getSimpleName()) {
+            case "Bulldozer" -> machineList = new BulldozerProducer().get();
+            case "Excavator" -> machineList = new ExcavatorProducer().get();
+            case "Truck" -> machineList = new TruckProducer().get();
+            default -> {
+                return new ArrayList<>();
+            }
+        } return new ArrayList<>(machineList);
     }
 
     @Override
